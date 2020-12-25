5931a958a8382a7281ff8e52eb4d49b1d745b9ab
aryan465 <aryananand567@gmail.com>
2020-12-26 00:23:38 +0530

    commit 1

---
+++ sudoku.py
@@ -0,0 +1,44 @@
+import math
+import numpy as np
+
+def possible(y, x, n):
+    global grid
+    for i in range(9):
+        if(grid[y][i] == n):
+            return False
+    for i in range(9):
+        if(grid[i][x] == n):
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(3):
+        for j in range(3):
+            if(grid[y0+i][x0+j] == n):
+                return False
+    return True  
+
+def solve():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if (grid[y][x] == 0):
+                for n in range(1,10):
+                    if possible(y,x,n):
+                        grid[y][x] = n
+                        solve()
+                        # print(np.matrix(grid))
+                        # input('enter')
+                        grid[y][x] = 0
+                return
+    print(np.matrix(grid))
+    input('More?')
+
+
+if __name__ == '__main__':
+    grid = list()
+    for i in range(9):
+        grid.append(list(map(int,input().split())))
+
+    print(np.matrix(grid))
+    print('@@@@@@@@@@#################@@@@@@@@@@@#########')
+    solve()
